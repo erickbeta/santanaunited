36a6a05c094b7d0a44ffb245b0f9e54dbcd9961c
Erick Betancourt Garcia <erickbeta12@gmail.com>
2025-09-13 12:34:10 -0600

    fist commit, creadas las migraciones y los modelos con sus propiedades fillable

---
+++ app/Models/Coach.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coach extends Model
+{
+    protected $fillable = [
+        'team_id',
+        'name',
+        'photo_path',
+    ];
+}
